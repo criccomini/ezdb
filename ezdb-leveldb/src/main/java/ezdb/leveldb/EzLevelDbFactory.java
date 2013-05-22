a038b5999be6df99bfc36105f4de03589ec2186d
Chris Riccomini <criccomi@criccomi-mn.linkedin.biz>
2013-05-22 12:22:39 -0700

    adding a bit of java docs

--- ezdb-leveldb/src/main/java/ezdb/leveldb/EzLevelDbFactory.java
+++ ezdb-leveldb/src/main/java/ezdb/leveldb/EzLevelDbFactory.java
@@ -5,6 +5,14 @@ import java.io.IOException;
 import org.iq80.leveldb.DB;
 import org.iq80.leveldb.Options;
 
+/**
+ * An interface that allows us to inject either a JNI or pure-Java
+ * implementation of LevelDB. EzLevelDb uses this class to open and delete
+ * LevelDb instances.
+ * 
+ * @author criccomi
+ * 
+ */
 public interface EzLevelDbFactory {
   public DB open(File path, Options options) throws IOException;
 
