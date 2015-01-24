87a75d2e7a7f0fc3dc574e7d8296928f716c2c72
subes <gsubes@gmail.com>
2015-01-25 00:42:53 +0100

    also testing midrange now

--- ezdb-leveldb-jni/src/test/java/ezdb/leveldb/TestStockData.java
+++ ezdb-leveldb-jni/src/test/java/ezdb/leveldb/TestStockData.java
@@ -26,7 +26,9 @@ import com.google.common.io.CharStreams;
 import ezdb.Db;
 import ezdb.RangeTable;
 import ezdb.TableIterator;
+import ezdb.TableRow;
 import ezdb.comparator.SerdeComparator;
+import ezdb.serde.DateSerde;
 import ezdb.serde.IntegerSerde;
 import ezdb.serde.LongSerde;
 import ezdb.serde.Serde;
@@ -42,15 +44,17 @@ public class TestStockData {
 			.getTime();
 
 	private final Serde<String> hashKeySerde = StringSerde.get;
-	private final Serde<Long> hashRangeSerde = LongSerde.get;
+	private final Serde<Date> hashRangeSerde = DateSerde.get;
 	private final Serde<Integer> valueSerde = SerializingSerde.get();
 
 	protected static final File ROOT = FileUtils
 			.createTempDir(TestEzLevelDb.class.getSimpleName());
 	protected Db ezdb;
-	protected RangeTable<String, Long, Integer> table;
-	private Comparator<byte[]> hashKeyComparator = new SerdeComparator<String>(hashKeySerde);
-	private Comparator<byte[]> rangeKeyComparator = new SerdeComparator<Long>(hashRangeSerde);
+	protected RangeTable<String, Date, Integer> table;
+	private Comparator<byte[]> hashKeyComparator = new SerdeComparator<String>(
+			hashKeySerde);
+	private Comparator<byte[]> rangeKeyComparator = new SerdeComparator<Date>(
+			hashRangeSerde);
 
 	@Before
 	public void before() {
@@ -89,17 +93,18 @@ public class TestStockData {
 			countDates++;
 			String dateStr = split[0];
 			Date date = df.parse(dateStr);
-			if(firstDate == null){
+			if (firstDate == null) {
 				firstDate = date;
 			}
 			lastDate = date;
 			long longTime = date.getTime();
 			if (prevLongTime != null) {
-//				System.out.println(dateStr + ":"+date + " - "+prevLongTime+"  < " + longTime + " -> "
-//						+ (prevLongTime < longTime));
+				// System.out.println(dateStr + ":"+date +
+				// " - "+prevLongTime+"  < " + longTime + " -> "
+				// + (prevLongTime < longTime));
 				Assert.assertTrue(prevLongTime < longTime);
 			}
-			table.put(MSFT, longTime, countDates);
+			table.put(MSFT, date, countDates);
 			prevLongTime = longTime;
 		}
 		System.out.println(MSFT + " has " + countDates + " bars");
@@ -109,24 +114,42 @@ public class TestStockData {
 	}
 
 	private void assertIteration(int countDates, Date fromDate, Date toDate) {
-		TableIterator<String, Long, Integer> range = table.range(MSFT,
-				fromDate.getTime(), toDate.getTime());
+		TableIterator<String, Date, Integer> range = table.range(MSFT,
+				fromDate, toDate);
 		int iteratedBars = 0;
 		int prevValue = 0;
+		Date left1000Date = null;
+		Date left900Date = null;
 		while (range.hasNext()) {
-			Integer value = range.next().getValue();
-//			System.out.println(value);
+			TableRow<String, Date, Integer> next = range.next();
+			Integer value = next.getValue();
+			// System.out.println(value);
 			iteratedBars++;
 			Assert.assertTrue(prevValue < value);
 			prevValue = value;
+			if (iteratedBars == countDates - 999) {
+				left1000Date = next.getRangeKey();
+			}
+			if (iteratedBars == countDates - 900) {
+				left900Date = next.getRangeKey();
+			}
 		}
 		Assert.assertEquals(countDates, iteratedBars);
 
-		Assert.assertEquals((Integer) 1,
-				table.getLatest(MSFT, fromDate.getTime()));
+		Assert.assertEquals((Integer) 1, table.getLatest(MSFT, fromDate));
 
-		Assert.assertEquals((Integer) countDates,
-				table.getLatest(MSFT, toDate.getTime()));
+		Assert.assertEquals((Integer) countDates, table.getLatest(MSFT, toDate));
+
+//		System.out.println(left1000Date +" -> "+left900Date);
+		range = table.range(MSFT, left1000Date, left900Date);
+		int curLeftIt = 0;
+		while (range.hasNext()) {
+			TableRow<String, Date, Integer> next = range.next();
+			curLeftIt++;
+			Assert.assertEquals((Integer) (countDates - 1000 + curLeftIt),
+					next.getValue());
+		}
+		Assert.assertEquals(100, curLeftIt);
 	}
 
 }
