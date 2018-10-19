92f85ee69e537246bd8e07a763c78fedde22f92d
subes <gsubes@gmail.com>
2018-10-19 14:32:14 +0200

    disable notls

--- ezdb-lmdb-jnr/src/main/java/ezdb/lmdb/EzLmDbTable.java
+++ ezdb-lmdb-jnr/src/main/java/ezdb/lmdb/EzLmDbTable.java
@@ -45,7 +45,7 @@ public class EzLmDbTable<H, R, V> implements RangeTable<H, R, V> {
 		this.rangeKeyComparator = rangeKeyComparator;
 
 		try {
-			this.env = factory.create(path.getParentFile(), EnvFlags.MDB_NOTLS);
+			this.env = factory.create(path.getParentFile());
 		} catch (IOException e) {
 			throw new DbException(e);
 		}
