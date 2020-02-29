05991114829e61995589caea744040f8e3c227a1
subes <gsubes@gmail.com>
2020-03-01 00:30:40 +0100

    no flip

--- ezdb-api/src/main/java/ezdb/serde/IntegerSerde.java
+++ ezdb-api/src/main/java/ezdb/serde/IntegerSerde.java
@@ -3,20 +3,18 @@ package ezdb.serde;
 import java.nio.ByteBuffer;
 
 public class IntegerSerde implements Serde<Integer> {
-  public static final IntegerSerde get = new IntegerSerde();
+	public static final IntegerSerde get = new IntegerSerde();
 
-  @Override
-  public Integer fromBytes(byte[] bytes) {
-    ByteBuffer buffer = ByteBuffer.allocate(4);
-    buffer.put(bytes);
-    buffer.flip();// need flip
-    return buffer.getInt();
-  }
+	@Override
+	public Integer fromBytes(final byte[] bytes) {
+		final ByteBuffer buffer = ByteBuffer.wrap(bytes);
+		return buffer.getInt();
+	}
 
-  @Override
-  public byte[] toBytes(Integer obj) {
-    ByteBuffer buffer = ByteBuffer.allocate(4);
-    buffer.putInt(obj);
-    return buffer.array();
-  }
+	@Override
+	public byte[] toBytes(final Integer obj) {
+		final ByteBuffer buffer = ByteBuffer.allocate(4);
+		buffer.putInt(obj);
+		return buffer.array();
+	}
 }
--- ezdb-api/src/main/java/ezdb/serde/LongSerde.java
+++ ezdb-api/src/main/java/ezdb/serde/LongSerde.java
@@ -3,20 +3,18 @@ package ezdb.serde;
 import java.nio.ByteBuffer;
 
 public class LongSerde implements Serde<Long> {
-  public static final LongSerde get = new LongSerde();
+	public static final LongSerde get = new LongSerde();
 
-  @Override
-  public Long fromBytes(byte[] bytes) {
-    ByteBuffer buffer = ByteBuffer.allocate(8);
-    buffer.put(bytes);
-    buffer.flip();// need flip
-    return buffer.getLong();
-  }
+	@Override
+	public Long fromBytes(final byte[] bytes) {
+		final ByteBuffer buffer = ByteBuffer.wrap(bytes);
+		return buffer.getLong();
+	}
 
-  @Override
-  public byte[] toBytes(Long obj) {
-    ByteBuffer buffer = ByteBuffer.allocate(8);
-    buffer.putLong(obj);
-    return buffer.array();
-  }
+	@Override
+	public byte[] toBytes(final Long obj) {
+		final ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.putLong(obj);
+		return buffer.array();
+	}
 }
