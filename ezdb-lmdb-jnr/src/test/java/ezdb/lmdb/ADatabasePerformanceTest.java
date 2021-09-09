b6ccd6ffa1e30c46736149d8b94d46251590ce62
subes <gsubes@gmail.com>
2021-09-09 23:03:40 +0200

    performance tests isolated

---
+++ ezdb-lmdb-jnr/src/test/java/ezdb/lmdb/ADatabasePerformanceTest.java
@@ -0,0 +1,16 @@
+package ezdb.lmdb;
+
+public abstract class ADatabasePerformanceTest {
+
+	protected static final int READS = 10;
+	protected static final int VALUES = 1_000_000;
+	protected static final String HASH_KEY = "HASH_KEY";
+	protected static final int FLUSH_INTERVAL = 10_000;
+
+	protected void printProgress(final String action, final long start, final double count, final double maxCount) {
+		final long duration = System.currentTimeMillis() - start;
+		System.out.println(action + ": " + count + "/" + maxCount + " (" + (count / maxCount * 100) + ") "
+				+ (count / duration) + "/ms during " + duration);
+	}
+
+}
---
+++ ezdb-lmdb-jnr/src/test/java/ezdb/lmdb/LmdbPerformanceTest.java
@@ -0,0 +1,137 @@
+package ezdb.lmdb;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import org.junit.Test;
+
+import ezdb.TableRow;
+import ezdb.comparator.LexicographicalComparator;
+import ezdb.lmdb.util.FileUtils;
+import ezdb.serde.LongSerde;
+import ezdb.serde.StringSerde;
+
+@NotThreadSafe
+//@Ignore("manual test")
+public class LmdbPerformanceTest extends ADatabasePerformanceTest {
+
+	protected static final File ROOT = FileUtils.createTempDir(TestEzLmDb.class.getSimpleName());
+
+	@Test
+	public void testLevelDbPerformance() {
+		final EzLmDbTable<String, Long, Long> table = new EzLmDbTable(ROOT, new EzLmDbJnrFactory(), StringSerde.get,
+				LongSerde.get, LongSerde.get, new LexicographicalComparator(), new LexicographicalComparator());
+
+		// RangeBatch<String, FDate, FDate> batch = table.newRangeBatch();
+		final long writesStart = System.currentTimeMillis();
+		for (long i = 0; i < VALUES; i++) {
+			table.put(HASH_KEY, i, i);
+			if (i % FLUSH_INTERVAL == 0) {
+				printProgress("Writes", writesStart, i, VALUES);
+				// try {
+				// batch.flush();
+				// batch.close();
+				// } catch (final IOException e) {
+				// throw new RuntimeException(e);
+				// }
+				// batch = table.newRangeBatch();
+			}
+		}
+		// try {
+		// batch.flush();
+		// batch.close();
+		// } catch (final IOException e) {
+		// throw new RuntimeException(e);
+		// }
+		printProgress("WritesFinished", writesStart, VALUES, VALUES);
+
+		readIterator(table);
+		readGet(table);
+		readGetLatest(table);
+	}
+
+	private void readIterator(final EzLmDbTable<String, Long, Long> table) {
+		final long readsStart = System.currentTimeMillis();
+		for (int reads = 1; reads <= READS; reads++) {
+			Long prevValue = null;
+			final Iterator<TableRow<String, Long, Long>> range = table.range(HASH_KEY);
+			int count = 0;
+			while (true) {
+				try {
+					final TableRow<String, Long, Long> value = range.next();
+//					if (prevValue != null) {
+//						if (prevValue >= value.getValue()) {
+//							throw new IllegalStateException(prevValue + " >= " + value.getValue());
+//						}
+//					}
+					prevValue = value.getValue();
+					count++;
+				} catch (final NoSuchElementException e) {
+					break;
+				}
+			}
+			printProgress("Reads", readsStart, VALUES * reads, VALUES * READS);
+		}
+		printProgress("ReadsFinished", readsStart, VALUES * READS, VALUES * READS);
+	}
+
+	private void readGet(final EzLmDbTable<String, Long, Long> table) {
+		final List<Long> values = newValues();
+		final long readsStart = System.currentTimeMillis();
+		for (int reads = 1; reads <= READS; reads++) {
+			Long prevValue = null;
+			for (int i = 0; i < values.size(); i++) {
+				try {
+					final Long value = table.get(HASH_KEY, values.get(i));
+//					if (prevValue != null) {
+//						if (prevValue >= value) {
+//							throw new IllegalStateException(prevValue + " >= " + value.getValue());
+//						}
+//					}
+					prevValue = value;
+				} catch (final NoSuchElementException e) {
+					break;
+				}
+			}
+			printProgress("Gets", readsStart, VALUES * reads, VALUES * READS);
+		}
+		printProgress("GetsFinished", readsStart, VALUES * READS, VALUES * READS);
+	}
+
+	private List<Long> newValues() {
+		final List<Long> values = new ArrayList<Long>();
+		for (long i = 0; i < VALUES; i++) {
+			values.add(i);
+		}
+		return values;
+	}
+
+	private void readGetLatest(final EzLmDbTable<String, Long, Long> table) {
+		final List<Long> values = newValues();
+		final long readsStart = System.currentTimeMillis();
+		for (int reads = 1; reads <= READS; reads++) {
+			Long prevValue = null;
+			for (int i = 0; i < values.size(); i++) {
+				try {
+					final TableRow<String, Long, Long> value = table.getLatest(HASH_KEY, values.get(i));
+//					if (prevValue != null) {
+//						if (prevValue >= value.getValue()) {
+//							throw new IllegalStateException(prevValue + " >= " + value.getValue());
+//						}
+//					}
+					prevValue = value.getValue();
+				} catch (final NoSuchElementException e) {
+					break;
+				}
+			}
+			printProgress("GetLatests", readsStart, VALUES * reads, VALUES * READS);
+		}
+		printProgress("GetLatestsFinished", readsStart, VALUES * READS, VALUES * READS);
+	}
+
+}
